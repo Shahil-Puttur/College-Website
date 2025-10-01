700facfc755cb460affd6ebdb1e3fcbaf3470c83
Shahil Puttur <rajubhai59647@gmail.com>
2025-10-01 09:48:22 +0530

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Paste your URL here
+const SUPABASE_ANON_KEY = 'YOUR_ANON_PUBLIC_KEY'; // Paste your Anon Key here
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
