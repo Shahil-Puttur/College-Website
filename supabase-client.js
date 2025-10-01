96e03300c1a2dc86ddec08cff670842be1336984
Shahil Puttur <rajubhai59647@gmail.com>
2025-10-01 10:09:05 +0530

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Paste your URL here
-const SUPABASE_ANON_KEY = 'YOUR_ANON_PUBLIC_KEY'; // Paste your Anon Key here
+const SUPABASE_URL = 'https://ynrbfvwrnujlkdepyzai.supabase.co'; // Paste your URL here
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlucmJmdndybnVqbGtkZXB5emFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyODQ1MzYsImV4cCI6MjA3NDg2MDUzNn0.FgCliv6gFdPXY3oKURnK2gRbTLD7XuDgviw-NGXHF98'; // Paste your Anon Key here
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
